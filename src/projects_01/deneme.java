700a3ab5c4949dc15934c065c8f7b2a4a6632371
GulsenGenc <gulsengenctuncer@gmail.com>
2022-03-28 14:39:36 +0300

    28.03.2022

--- src/projects_01/deneme.java
+++ src/projects_01/deneme.java
@@ -4,7 +4,7 @@ import javax.crypto.spec.PSource;
 
 public class deneme {
     public static void main(String[] args){
-        System.out.println();
+        System.out.println("gülşen");
 
     }
 }
