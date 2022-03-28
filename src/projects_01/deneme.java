d2bb9ddecc708751ab5a58c0d8314ba33cac787f
filiz kodat <filizkodat0707@gmail.com>
2022-03-29 00:41:24 +0300

    29.03.2022-filiz

--- src/projects_01/deneme.java
+++ src/projects_01/deneme.java
@@ -1,4 +1,12 @@
 package projects_01;
 
 public class deneme {
+    public static void main(String[] args) {
+        System.out.println("gülşen");
+        System.out.println("selçuk");
+        System.out.println("proje");
+        System.out.println("kadir");
+        System.out.println("filiz");
+
+    }
 }
