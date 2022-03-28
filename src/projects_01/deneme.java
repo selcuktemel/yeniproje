3b39b5bb8ff9b415276bfc6f07dfc8624f7f1142
GulsenGenc <gulsengenctuncer@gmail.com>
2022-03-28 14:52:28 +0300

    28..03.2022

--- src/projects_01/deneme.java
+++ src/projects_01/deneme.java
@@ -6,6 +6,7 @@ public class deneme {
     public static void main(String[] args){
         System.out.println("gülşen");
         System.out.println("selçuk");
+        System.out.println("proje");
 
     }
 }
