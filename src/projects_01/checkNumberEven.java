4b38eb832679a94475249ef5aebb32794881c72f
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-03-29 15:45:56 +0300

    "29.03.2022"

--- src/projects_01/checkNumberEven.java
+++ src/projects_01/checkNumberEven.java
@@ -36,6 +36,7 @@ public class checkNumberEven {
         //  code Start here don't change before this line
         // Kodu burdan baslatin ve bu satırdan önceki kodlari değiştirmeyin
 
+
         if(number%2==0){
             System.out.println(true);
         }else{
